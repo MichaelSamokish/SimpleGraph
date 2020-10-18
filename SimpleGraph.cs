ff524d358ebfc6225cd5ce37d1f5434f60a63ac3
GODZILLA <samich2006@gmail.com>
2020-10-18 13:14:16 +0300

    Test commit

--- SimpleGraph.cs
+++ SimpleGraph.cs
@@ -79,6 +79,7 @@ namespace SimpleGraph
                 if(!visited[nextNode])
                 {
                     DepthFirstSearch(nextNode, visited, stack);
+
                 }
             }
 
